e678a874b1c134bd244c0ff63d621cb2bcbdfae7
ralfk <ralfk@MORNINGSTAR>
2024-02-02 12:46:57 +0100

    Projektdateien hinzufügen.

---
+++ Translator_H/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Translator_H
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(375, 147);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Deutsch -> Hells";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(-10, 441);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(375, 161);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Hells  -> Deutsch";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(455, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(460, 226);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox2.Location = new System.Drawing.Point(455, 282);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(460, 217);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft New Tai Lue", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox3.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox3.Location = new System.Drawing.Point(455, 533);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(460, 249);
+            this.richTextBox3.TabIndex = 11;
+            this.richTextBox3.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = global::Translator_H.Properties.Resources.OIG;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(74, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(378, 808);
+            this.panel2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = global::Translator_H.Properties.Resources.OIG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1100, 835);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.Text = "Übersetzer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Translator_H/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translator_H
+{
+    public partial class Form1 : Form
+    {
+        // Definiere die Methode, die ein Dictionary zurückgibt
+        public Dictionary<string, string> CreateDictionary()
+        {
+            // Erstelle ein Dictionary, das die Buchstaben aus der Spalte Deutsch als Schlüssel und die Buchstaben aus der Spalte Hells als Wert verwendet
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            // Erstelle einen StreamReader, der die alphabet.csv-Datei öffnet
+            StreamReader reader = new StreamReader(Environment.CurrentDirectory + @"\alphabet.csv");
+
+            // Lese die erste Zeile der Datei (die Überschriften) und ignoriere sie
+            reader.ReadLine();
+
+            // Lese die restlichen Zeilen der Datei in einer Schleife
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                // Teile die Zeile an dem Komma auf, um die beiden Buchstaben zu erhalten
+                string[] parts = line.Split(';');
+
+                // Füge das Schlüssel-Wert-Paar dem Dictionary hinzu
+                dict.Add(parts[0], parts[1]);
+            }
+
+            // Schließe den StreamReader
+            reader.Close();
+
+            // Gib das Dictionary zurück
+            return dict;
+        }
+
+        // Definiere die Methode, die ein umgekehrtes Dictionary zurückgibt
+        public Dictionary<string, string> CreateReverseDictionary()
+        {
+            // Erstelle ein Dictionary, das die Buchstaben aus der Spalte Hells als Schlüssel und die Buchstaben aus der Spalte Deutsch als Wert verwendet
+            Dictionary<string, string> reverseDict = new Dictionary<string, string>();
+
+            // Erstelle einen StreamReader, der die alphabet.csv-Datei öffnet
+            StreamReader reader = new StreamReader(Environment.CurrentDirectory + @"\alphabet.csv");
+
+            // Lese die erste Zeile der Datei (die Überschriften) und ignoriere sie
+            reader.ReadLine();
+
+            // Lese die restlichen Zeilen der Datei in einer Schleife
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                // Teile die Zeile an dem Komma auf, um die beiden Buchstaben zu erhalten
+                string[] parts = line.Split(';');
+
+                // Füge das Schlüssel-Wert-Paar dem Dictionary hinzu
+                reverseDict.Add(parts[1], parts[0]);
+            }
+
+            // Schließe den StreamReader
+            reader.Close();
+
+            // Gib das Dictionary zurück
+            return reverseDict;
+        }
+
+        // Deklariere eine Variable für den StreamReader
+        StreamReader reader;
+
+        // Deklariere eine Variable für das Dictionary
+        Dictionary<string, string> dict;
+
+        // Deklariere eine Variable für das umgekehrte Dictionary
+        Dictionary<string, string> reverseDict;
+
+        // Deklariere eine Variable für den Dateinamen
+        string fileName;
+
+        // Deklariere eine Variable für den Pfad
+         string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+            // Initialisiere den StreamReader mit dem Pfad zu deiner Datei
+            reader = new StreamReader(Environment.CurrentDirectory + @"\alphabet.csv");
+
+            // Initialisiere das Dictionary mit der Methode, die du geschrieben hast
+            dict = CreateDictionary();
+
+            // Initialisiere das umgekehrte Dictionary mit der Methode, die du geschrieben hast
+            reverseDict = CreateReverseDictionary();
+
+            // Ändere die Höhe der richTextBox3 auf 50
+            richTextBox3.Height = 50;
+
+            // Erstelle einen Dateinamen aus dem aktuellen Datum und der aktuellen Uhrzeit
+            fileName = DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+
+            // Erstelle einen Pfad zu dem Ordner, in dem die Datei gespeichert werden soll
+            _ = Environment.CurrentDirectory + @"";
+            // Erstelle einen Pfad zu dem Programmordner
+
+        }
+        private void SaveMyFile()
+        {
+            // Create a SaveFileDialog to request a path and file name to save to.
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+
+            // Initialize the SaveFileDialog to specify the RTF extention for the file.
+            saveFile1.DefaultExt = "*.rtf";
+            saveFile1.Filter = "RTF Files|*.rtf";
+
+            // Determine whether the user selected a file name from the saveFileDialog.
+            if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               saveFile1.FileName.Length > 0)
+            {
+                // Save the contents of the RichTextBox into the file.
+                richTextBox1.SaveFile(saveFile1.FileName);
+            }
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Lese die Eingabe aus der richTextBox1
+            string input = richTextBox1.Text;
+
+            // Erstelle eine Variable für die Ausgabe
+            string output = "";
+
+            // Übersetze die Eingabe mit dem Dictionary in einer Schleife
+            foreach (char c in input)
+            {
+                // Prüfe, ob der Buchstabe im Dictionary vorhanden ist
+                if (dict.ContainsKey(c.ToString()))
+                {
+                    // Ersetze den Buchstaben mit dem entsprechenden Zeichen aus dem Dictionary
+                    output += dict[c.ToString()];
+                }
+                else
+                {
+                    // Lasse den Buchstaben unverändert
+                    output += c;
+                }
+            }
+
+            // Zeige die Ausgabe in der richTextBox2 an
+            richTextBox2.Text = output;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveMyFile();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Lese die Eingabe aus der richTextBox2
+            string input = richTextBox2.Text;
+
+            // Erstelle eine Variable für die Ausgabe
+            string output = "";
+
+            // Übersetze die Eingabe mit dem umgekehrten Dictionary in einer Schleife
+            foreach (char c in input)
+            {
+                // Prüfe, ob der Buchstabe im umgekehrten Dictionary vorhanden ist
+                if (reverseDict.ContainsKey(c.ToString()))
+                {
+                    // Ersetze den Buchstaben mit dem entsprechenden Zeichen aus dem umgekehrten Dictionary
+                    output += reverseDict[c.ToString()];
+                }
+                else
+                {
+                    // Lasse den Buchstaben unverändert
+                    output += c;
+                }
+            }
+
+            // Zeige die Ausgabe in der richTextBox3 an
+            richTextBox3.Text = output;
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        // Definiere die Methode, die die Ausgabe von button1 in die Datei schreibt
+
+    }
+}
+
